b383a8261dbba138ba14b95e6088deebbc6f9453
Arthur Noreika <arthurnoreika11@live.com>
2026-03-19 15:01:27 +0000

    Created some variables and combined them to produce a summary string which is printed when the file is ran

--- parser.py
+++ parser.py
@@ -1,1 +1,8 @@
 #Legal Document Praser - Start of my legal tech jounery
+client = "Henderson & Sons"
+contractValue = 48500
+contractSigned = True
+contractStartDate = "19-03-2026"
+
+summary = f"Contract with {client} (£{contractValue} was signed and will start on {contractStartDate}."
+print(summary)
